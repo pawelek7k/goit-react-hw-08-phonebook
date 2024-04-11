64639018bf9379e700828b90abbae6fa581eba69
pawelek <k.pawelek@vp.pl>
2024-04-11 21:13:45 +0200

    add nanoid & first code

--- src/components/FormComponent/Form.jsx
+++ src/components/FormComponent/Form.jsx
@@ -1,23 +1,82 @@
-import PropTypes from "prop-types";
+import { nanoid } from "nanoid";
+import { useState } from "react";
 
 export const Form = () => {
+  const [contacts, setContacts] = useState([]);
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNumber(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newContact = {
+      id: nanoid(),
+      name: name,
+      number: number,
+    };
+    setContacts([...contacts, newContact]);
+    setName("");
+    setNumber("");
+  };
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <>
-      <div>
+    <div>
+      <h1>Contacts</h1>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          placeholder="Enter name"
+          value={name}
+          onChange={handleNameChange}
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
+        <input
+          type="tel"
+          name="number"
+          placeholder="Enter number"
+          value={number}
+          onChange={handleNumberChange}
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
         <button type="submit">Add contact</button>
-      </div>
-    </>
+      </form>
+      <input
+        type="text"
+        placeholder="Search contacts"
+        value={filter}
+        onChange={handleFilterChange}
+      />
+      <ul>
+        {filteredContacts.map((contact) => (
+          <li key={contact.id}>
+            {contact.name} - {contact.number}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
-Form.propTypes = {
-  nameValueInput: PropTypes.string.isRequired,
-  numberValueInput: PropTypes.number.isRequired,
-};
+// Form.propTypes = {
+//   nameValueInput: PropTypes.string.isRequired,
+//   numberValueInput: PropTypes.number.isRequired,
+// };
