000c3a31ccfb41ad8afb6cf3a6adf0afe20bff42
pawelek <k.pawelek@vp.pl>
2024-04-11 22:15:55 +0200

    add fixes

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -3,11 +3,11 @@ import PropTypes from "prop-types";
 export const ContactList = ({ contacts }) => {
   return (
     <ul>
-      {contacts.map((contact) => {
+      {contacts.map((contact) => (
         <li key={contact.id}>
           {contact.name} - {contact.number}
-        </li>;
-      })}
+        </li>
+      ))}
     </ul>
   );
 };
