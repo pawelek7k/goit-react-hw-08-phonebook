2470f0ee38199b9c2d2a3701779024f862fc265e
pawelek <k.pawelek@vp.pl>
2024-05-28 19:48:55 +0200

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
-import { LoginForm } from "components/LoginForm/LoginForm";
 import { Helmet } from "react-helmet";
+import { LoginForm } from "../components/LoginForm/LoginForm";
 
 export default function Login() {
   return (
