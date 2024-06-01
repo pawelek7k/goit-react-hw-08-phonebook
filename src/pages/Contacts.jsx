37304a9ddf99833beb921c786d06d3db9d3e06f3
pawelek <k.pawelek@vp.pl>
2024-06-01 18:38:36 +0200

    Update Contacts.jsx

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { Helmet } from "react-helmet";
 import { useDispatch, useSelector } from "react-redux";
 import { ContactList } from "../components/ContactsList/ContactsList";
+import Filter from "../components/Filter/Filter";
 import { ContactForm } from "../components/FormComponent/ContactForm";
 import { fetchContacts } from "../stores/contacts/api";
 import { selectIsLoading } from "../stores/contacts/selectors";
@@ -21,6 +22,7 @@ export default function Contacts() {
       </Helmet>
       <ContactForm />
       <div>{isLoading && "Request in progress..."}</div>
+      <Filter />
       <ContactList />
     </>
   );
