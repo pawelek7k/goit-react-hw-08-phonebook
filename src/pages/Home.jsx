c478fb8541a2ffb477824ed2f3802eab39e684a0
pawelek <k.pawelek@vp.pl>
2024-06-02 12:33:36 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>
-        Task manager welcome page{" "}
+        Welcome to contacts app!{" "}
         <span role="img" aria-label="Greeting icon">
           💁‍♀️
         </span>
